02cfd277b4da3fd09047b113c2bc5760da62f69e
Jeremy <jeremy.bidet@gmail.com>
2014-12-12 09:54:02 +0100

    same as last

--- src/fr/upem/main/Main.java
+++ src/fr/upem/main/Main.java
@@ -5,8 +5,6 @@ import fr.umlv.zen3.Application;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world !");
 		
 		Application.run("Hello app", 800, 600, __ -> System.out.println("Hello world in app !"));
 	}
