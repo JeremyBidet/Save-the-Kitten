bc93e4aabf3628a5b25377df96b23b98fbbdbab2
Jeremy <jeremy.bidet@gmail.com>
2014-12-12 09:50:57 +0100

    Discover Zen framework

--- src/fr/upem/main/Main.java
+++ src/fr/upem/main/Main.java
@@ -1,10 +1,14 @@
 package fr.upem.main;
 
+import fr.umlv.zen3.Application;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world !");
+		
+		Application.run("Hello app", 800, 600, __ -> System.out.println("Hello world in app !"));
 	}
 
 }
